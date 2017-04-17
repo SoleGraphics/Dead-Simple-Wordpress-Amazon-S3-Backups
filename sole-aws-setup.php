6284d99fe82f1f82e2d76dc443e3cef5879c5148
Ben Greene <ben@solegraphics.com>
2017-04-17 16:20:39 -0700

    feat: settings page
    Added settings for backup frequencies and times.
    Added options for making settings dropdowns instead of text fields
    Added manual backup button to the settings page (non-functional)

--- sole-aws-setup.php
+++ sole-aws-setup.php
@@ -25,6 +25,20 @@ class Sole_AWS_Backup {
 		'Access Secret' => 'sole_aws_access_secret',
 		'Bucket'        => 'sole_aws_bucket',
 		'Region'        => 'sole_aws_region',
+		'Database Backup Frequency' => array(
+			'slug'    => 'sole_aws_db_backup_frequency',
+			'options' => array(
+				'daily', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday',
+			)
+		),
+		'Database Backup Time' => 'sole_aws_db_backup_timestamp',
+		'Uploads Backup Frequency' => array(
+			'slug'    => 'sole_aws_db_uploads_frequency',
+			'options' => array(
+				'daily', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday',
+			)
+		),
+		'Uploads Backup Time' => 'sole_aws_uploads_backup_timestamp',
 	);
 
 	function __construct() {
@@ -35,11 +49,17 @@ class Sole_AWS_Backup {
 		// TODO: add the cron jobs
 	}
 
+	// Setup the menu in the admin panel
 	public function add_admin_menu() {
-		add_menu_page( 'Dead Simple Backup Settings', 'Dead Simple Backup Settings', 'administrator', self::SETTINGS_PAGE_SLUG, array( $this, 'display_settings_page' ), 'dashicons-analytics' );
+		add_menu_page( 'Dead Simple Backup', 'Dead Simple Backup', 'administrator', self::SETTINGS_PAGE_SLUG, '', 'dashicons-analytics' );
+
+		// Register submenu for plugin settings - default page for the plugin
+		add_submenu_page( self::SETTINGS_PAGE_SLUG, 'Dead Simple Backup Settings', 'Settings', 'administrator', self::SETTINGS_PAGE_SLUG, array( $this, 'display_settings_page' ) );
 	}
 
+	// Need to register all the settings
 	public function register_plugin_settings() {
+		// Settings defined at top of class
 		foreach ( $this->plugin_settings as $setting ) {
 			register_setting( self::SETTINGS_GROUP, $setting );
 		}
--- templates/settings-form.php
+++ templates/settings-form.php
@@ -3,16 +3,31 @@
 	<form method="POST" action="options.php">
 		<?php settings_fields( 'sole_aws_simple_backup_fields' ); ?>
 		<?php do_settings_sections( 'sole-settings-page' ); ?>
+		<h2>Settings</h2>
 		<table>
 			<?php foreach ( $this->plugin_settings as $name => $option ) { ?>
 				<tr>
 					<td><?php echo $name; ?></td>
 					<td>
-						<input type="text" name="<?php echo $option; ?>" value="<?php echo get_option( $option ); ?>" />
+						<?php if( 'array' == gettype( $option ) ): ?>
+							<select>
+								<option name="<?php echo $option['slug']; ?>" value="">Select</option>
+								<?php foreach( $option['options'] as $dd_option ): ?>
+									<option name="<?php echo $option['slug']; ?>" value="<?php echo $dd_option; ?>"><?php echo ucwords( $dd_option ); ?></option>
+								<?php endforeach; ?>
+							</select>
+						<?php else: ?>
+							<input type="text" name="<?php echo $option; ?>" value="<?php echo get_option( $option ); ?>" />
+						<?php endif; ?>
 					</td>
 				</tr>
 			<?php } ?>
 		</table>
 		<?php submit_button(); ?>
+	</form>
+	<h2>Extras</h2>
+	<form method="POST" action="options.php">
+		<input type="hidden" name="manual-sole-backup-trigger" value="true" />
+		<?php submit_button( 'Backup Files & Database' ); ?>
 	</form>
 </div>
